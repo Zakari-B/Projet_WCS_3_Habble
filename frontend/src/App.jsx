1abd4d48f6038d34fa1492f8296ecbc684a6a58b
Lora Perrichon <loraperrichon@gmail.com>
2022-05-25 15:30:26 +0200

    template chakra installation + buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import Home from "@pages/Home";
-
+import { Box, Button } from "@chakra-ui/react";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Box bg="pink.lighter">Welcome</Box>
+      <Button variant="solid_PrimaryColor">coucou</Button>
+      <Button variant="solid_SecondaryColor">coucou</Button>
       <p>coucou</p>
     </div>
   );
