6330133c661469e6f18ccc78ae671432103c79b1
MARIE SERRADORI <marie.serradori@gmail.com>
2022-05-30 09:56:00 +0200

    fixed register root to redirect to form based on search params

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,5 @@
+import { Link, useSearchParams } from "react-router-dom";
+
 import {
   Box,
   Flex,
@@ -10,62 +12,85 @@ import {
 
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer";
+import SignupForm from "../components/SignupForm";
 
 export default function Register() {
+  const [searchParams] = useSearchParams();
+  const role = searchParams.get("role");
+
   return (
     <Box h="100vh">
       <Header onDark={false} isSticky={false} />
-
-      <Flex bgColor="background.gray" alignItems="center">
-        <Flex
-          direction="column"
-          bgColor="white"
-          w={{ sm: "100%", md: "750px" }}
-          m="auto"
-          alignItems="center"
-          boxShadow="0px 1px 1px 0px rgb(185 184 184 / 75%)"
-          padding="40px 0px 0px 0px"
-          marginTop="100px"
-        >
-          <Heading as="h2" size="md" color="purple.average">
-            Que cherchez vous ?
-          </Heading>
-          <HStack
-            paddingY="60px"
-            justify="center"
-            alignItems="flex-start"
-            wrap="wrap"
+      {role === "freelancer" || role === "employer" ? (
+        <SignupForm />
+      ) : (
+        <Flex bgColor="background.gray" alignItems="center">
+          <Flex
+            direction="column"
+            bgColor="white"
+            w={{ sm: "100%", md: "750px" }}
+            m="auto"
+            alignItems="center"
+            boxShadow="0px 1px 1px 0px rgb(185 184 184 / 75%)"
+            padding="40px 0px 0px 0px"
+            marginY="100px"
           >
-            <VStack
-              w={{ sm: "100%", md: "40%" }}
-              p="20px"
-              alignItems="center"
-              borderRight={{ base: "none", md: "1px solid #eee" }}
-              borderBottom={{ base: "1px solid #eee", md: "none" }}
+            <Heading as="h2" size="md" color="purple.average">
+              Que cherchez vous ?
+            </Heading>
+            <HStack
+              paddingY="60px"
+              justify="center"
+              alignItems="flex-start"
+              wrap="wrap"
             >
-              <Text color="#415161" fontWeight="600">
-                Une aide
-              </Text>
-              <Text color="#415161">
-                Déposez une annonce, recevez des propositions personnalisées,
-                discutez par messagerie privée des détails, choisissez le ou les
-                professionnel(s), profitez !
-              </Text>
-              <Button variant="solid_PrimaryColor">S'INSCRIRE</Button>
-            </VStack>
-            <VStack p="20px" w={{ sm: "100%", md: "40%" }} alignItems="center">
-              <Text color="#415161" fontWeight="600">
-                Un job
-              </Text>
-              <Text color="#415161">
-                Créer un compte professionnel pour trouver des projets de
-                compensation du handicap intéressants et travailler
-              </Text>
-              <Button variant="solid_PrimaryColor">S'INSCRIRE</Button>
-            </VStack>
-          </HStack>
+              <VStack
+                w={{ sm: "100%", md: "40%" }}
+                p="20px"
+                alignItems="center"
+                borderRight={{ base: "none", md: "1px solid #eee" }}
+                borderBottom={{ base: "1px solid #eee", md: "none" }}
+              >
+                <Text color="#415161" fontWeight="700">
+                  Une aide
+                </Text>
+                <Text color="#415161">
+                  Déposez une annonce, recevez des propositions personnalisées,
+                  discutez par messagerie privée des détails, choisissez le ou
+                  les professionnel(s), profitez !
+                </Text>
+                <Link to="/register/?role=employer">
+                  <Button
+                    variant="solid_PrimaryColor"
+                    src="/register/?role=employer"
+                    fontWeight="700"
+                  >
+                    S'INSCRIRE
+                  </Button>
+                </Link>
+              </VStack>
+              <VStack
+                p="20px"
+                w={{ sm: "100%", md: "40%" }}
+                alignItems="center"
+              >
+                <Text color="#415161" fontWeight="700">
+                  Un job
+                </Text>
+                <Text color="#415161">
+                  Créer un compte professionnel pour trouver des projets de
+                  compensation du handicap intéressants et travailler
+                </Text>
+                <Link to="/register/?role=freelancer">
+                  <Button variant="solid_PrimaryColor" fontWeight="700">
+                    S'INSCRIRE
+                  </Button>
+                </Link>
+              </VStack>
+            </HStack>
+          </Flex>
         </Flex>
-      </Flex>
+      )}
       <Footer />
     </Box>
   );
