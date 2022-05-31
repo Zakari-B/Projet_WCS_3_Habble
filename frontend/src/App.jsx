4e5cac14234134d02563cfebd81c304e8ebd9ecd
MARIE SERRADORI <marie.serradori@gmail.com>
2022-05-31 17:26:37 +0200

    added the remove on event listener

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
-
+// import MentionsLegales from "./pages/MentionsLegales";
 import "./App.css";
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
         {/* <Route path="/le-projet" element={<Mission />} />
         <Route path="/contact" element={<Contact />} /> */}
         {/* <Route path="/professionnel-handicap" element={<ProAccueil />} /> */}
-        {/* <Route path="/mentions-legales" element={<MentionsLegales />} />
-        <Route path="/confidentialite" element={<Confidentialite />} /> */}
+        {/* <Route path="/mentions-legales" element={<MentionsLegales />} /> */}
+        {/* <Route path="/confidentialite" element={<Confidentialite />} /> */}
         <Route path="/register" element={<Register />} />
         {/* <Route path="/login" element={<LoginForm />} /> */}
         {/* <Route path="*" element={<Error />} /> */}
