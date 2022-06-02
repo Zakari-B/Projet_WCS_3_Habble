4075d61be45e3353f06b3ca37889bc1a55702e1e
MARIE SERRADORI <marie.serradori@gmail.com>
2022-06-02 15:55:14 +0200

    created pro page and route

--- frontend/src/pages/ProfilPageProfessional.jsx
+++ frontend/src/pages/ProfilPageProfessional.jsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer";
 
@@ -8,12 +8,8 @@ export default function Confidentialite() {
       <Header onDark={false} isSticky={false} />
       <Flex bgColor="background.gray" direction="column" justify="flex-start">
         <Flex w="70%" gap="20px" m="auto" paddingY="30px">
-          <Flex bgColor="red" minW="33%">
-            <Text>Lelelel</Text>
-          </Flex>
-          <Flex bgColor="blue" minW="66%">
-            <Text>Lalala</Text>
-          </Flex>
+          <Flex bgColor="red" minW="33%" minH="20vh" />
+          <Flex bgColor="blue" minW="66%" />
         </Flex>
       </Flex>
       <Footer />
