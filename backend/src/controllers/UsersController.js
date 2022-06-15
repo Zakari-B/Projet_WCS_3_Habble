52f6f7c51b5a8ec7342fb43705320857bdb96029
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-06-15 18:10:34 +0200

    post user route functional

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -1,42 +1,34 @@
 const { Prisma } = require("@prisma/client");
 const { hashPassword } = require("../helpers/argonHelper");
 const user = require("../models/user");
-const freelancer = require("../models/freelancer");
-const employer = require("../models/employer");
 
-const createOne = async (req, res) => {
-  let userCreated;
-  let freelancerCreated;
-  let employerCreated;
+const createOne = async (req, res, next) => {
   const { firstname, lastname, email, password, role } = req.body;
-  const completedProfile = !(role === "freelancer");
-  const error = user.validate(
-    {
-      firstname,
-      lastname,
-      email,
-      password,
-      role,
-      profileIsComplete: completedProfile,
-    },
-    true
-  );
+  const completedProfile = role !== "freelancer";
+  const error = user.validate({
+    firstname,
+    lastname,
+    email,
+    password,
+    role,
+    profileIsComplete: completedProfile,
+  });
   if (error) {
     res.status(422).json(error);
   }
   if (role === "freelancer" || role === "employer") {
     try {
-      const hashedPassword = (await hashPassword(password)).toString();
+      const hashedPassword = await hashPassword(password);
       const message = await user.createOne({
         firstname,
         lastname,
         email,
-        hashedPassword,
+        hashedPassword: hashedPassword.toString(),
         role,
         profileIsComplete: completedProfile,
-        dateCreated: new Date(),
       });
-      userCreated = message;
+      req.userCreated = message;
+      next();
     } catch (e) {
       if (e instanceof Prisma.PrismaClientKnownRequestError) {
         if (e.code === "P2002") {
@@ -46,45 +38,6 @@ const createOne = async (req, res) => {
         }
       }
     }
-  }
-  if (role === "freelancer") {
-    try {
-      const freelancerMessage = await freelancer.createOne({
-        displayName: `${userCreated.firstname} ${userCreated.lastname}`,
-        activityDescription: "",
-        userId: userCreated.id,
-        zipCode: "",
-        phone: "",
-        experienceYear: 0,
-        price: 0.0,
-        description: "",
-        acceptEmails: false,
-        siret: 0,
-        available: false,
-        dateCreated: new Date(),
-        picture: "",
-      });
-      freelancerCreated = freelancerMessage;
-    } catch (e) {
-      console.error({ error: "Problème de création de l'entrée freelancer" });
-    }
-    res.status(201).json({ userCreated, freelancerCreated });
-  } else if (role === "employer") {
-    try {
-      const employerMessage = await employer.createOne({
-        displayName: `${userCreated.firstname} ${userCreated.lastname}`,
-        userId: userCreated.id,
-        phone: "",
-        dateCreated: new Date(),
-        picture: "",
-      });
-      employerCreated = employerMessage;
-    } catch (e) {
-      console.error({ error: "Problème de création de l'entrée employeur" });
-    }
-    res.status(201).json({ userCreated, employerCreated });
-  } else {
-    res.status(400).send({ error: "Erreur d'attribution du rôle" });
   }
   return null;
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const UserController = require("./controllers/UsersController");
+const FreelancerController = require("./controllers/FreelancerController");
+const EmployerController = require("./controllers/EmployerController");
 
 const router = express.Router();
 
-router.post("/users", UserController.createOne);
+router.post(
+  "/users",
+  UserController.createOne,
+  FreelancerController.createOne,
+  EmployerController.createOne
+);
 
 module.exports = router;
