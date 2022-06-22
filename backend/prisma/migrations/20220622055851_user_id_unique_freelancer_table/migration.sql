5f88623b3bbf497477ed3357e709a340f17d38f1
Julie Combeau <combeau.julie@gmail.com>
2022-06-22 16:50:28 +0200

    remove last migrate prisma

--- backend/prisma/migrations/20220622055851_user_id_unique_freelancer_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `freelancer` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `freelancer_userId_key` ON `freelancer`(`userId`);
