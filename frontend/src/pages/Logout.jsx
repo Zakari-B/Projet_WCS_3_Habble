d6dcd8ced5cd66e96a7a765357e2aba5ff7ba073
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-06-23 16:27:16 +0200

    added spinner to logout page

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Spinner, Flex } from "@chakra-ui/react";
 import backendAPI from "../services/backendAPI";
 
 function Logout() {
@@ -11,7 +12,13 @@ function Logout() {
       navigate("/");
     });
   }, []);
-  return <div />;
+  return (
+    <div>
+      <Flex justify="center" alignItems="center" h="100vh">
+        <Spinner color="pink.average" size="xl" />
+      </Flex>
+    </div>
+  );
 }
 
 export default Logout;
