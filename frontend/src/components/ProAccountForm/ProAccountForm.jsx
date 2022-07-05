455969c7a023362cef0593855e173bfd376ebc00
Julie Combeau <combeau.julie@gmail.com>
2022-07-05 11:29:05 +0200

    change call axios to recover user ans add component for the picture

--- frontend/src/components/ProAccountForm/ProAccountForm.jsx
+++ frontend/src/components/ProAccountForm/ProAccountForm.jsx
@@ -9,7 +9,6 @@ import {
   Button,
   Text,
   Divider,
-  Avatar,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
@@ -18,15 +17,6 @@ import {
   Textarea,
   Checkbox,
   Link,
-  Box,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  Image,
   useToast,
 } from "@chakra-ui/react";
 
@@ -35,19 +25,19 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Services from "./Services";
 import Epertises from "./Expertises";
+import PictureProfilePro from "./PictureProfilPro";
 import backendAPI from "../../services/backendAPI";
 
 export default function ProAccountForm() {
-  const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
   const navigate = useNavigate();
   const toast = useToast();
   const { freelancerId } = useParams();
   // useState pour chaque input //
+  const [user, setUser] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [activityPro, setActivityPro] = useState("");
   const [cityPro, setCityPro] = useState("");
   const [phonePro, setPhonePro] = useState("");
-  const [picturePro, setPicturePro] = useState();
   const [experienceYearPro, setExperienceYearPro] = useState();
   const [pricePro, setPricePro] = useState();
   const [descriptionPro, setDescriptionPro] = useState("");
@@ -55,102 +45,98 @@ export default function ProAccountForm() {
 
   const [siretPro, setSiretPro] = useState();
 
-  // fonction d'enregistrement d'une nouvelle image //
-  const handleRegisterPicture = () => {
-    setPicturePro(picturePro);
-    onToggle(!isOpen);
+  // Appel axios pour récuperer un user
+
+  const getOneUser = () => {
+    backendAPI.get(`/api/freelancers/${freelancerId}/user`).then((response) => {
+      setUser(response.data);
+      setDisplayName(
+        response.data.freelancer[0].displayName === "undefined"
+          ? ""
+          : response.data.freelancer[0].displayName
+      );
+      setActivityPro(
+        response.data.freelancer[0].activityDescription === "undefined"
+          ? ""
+          : response.data.freelancer[0].activityDescription
+      );
+      setCityPro(
+        response.data.freelancer[0].zipCode === "undefined"
+          ? ""
+          : response.data.freelancer[0].zipCode
+      );
+      setPhonePro(response.data.freelancer[0].phone);
+      setExperienceYearPro(
+        response.data.freelancer[0].experienceYear === 0
+          ? ""
+          : response.data.freelancer[0].experienceYear
+      );
+      setPricePro(
+        response.data.freelancer[0].price === 0
+          ? ""
+          : response.data.freelancer[0].price
+      );
+      setDescriptionPro(
+        response.data.freelancer[0].description === "undefined"
+          ? ""
+          : response.data.freelancer[0].description
+      );
+      setAcceptEmailPro(response.data.freelancer[0].acceptEmail);
+      setSiretPro(response.data.freelancer[0].siret);
+    });
   };
 
-  // Appel axios pour récuperer le displayName du freelancer
-
-  const getOnefreelancer = () => {
-    backendAPI
-      .get(`/api/freelancers/${freelancerId}`)
-      .then((response) => {
-        setDisplayName(
-          response.data.displayName === "undefined"
-            ? ""
-            : response.data.displayName
-        );
-        setActivityPro(
-          response.data.activityDescription === "undefined"
-            ? ""
-            : response.data.activityDescription
-        );
-        setCityPro(
-          response.data.zipCode === "undefined" ? "" : response.data.zipCode
-        );
-        setPhonePro(response.data.phone);
-        setExperienceYearPro(
-          response.data.experienceYear === 0 ? "" : response.data.experienceYear
-        );
-        setPricePro(response.data.price === 0 ? "" : response.data.price);
-        setDescriptionPro(
-          response.data.description === "undefined"
-            ? ""
-            : response.data.description
-        );
-        setAcceptEmailPro(response.data.acceptEmail);
-        setSiretPro(response.data.siret);
-      })
-      .catch((error) => {
-        console.warn(error);
-      });
-  };
-
-  useEffect(() => getOnefreelancer(), []);
+  useEffect(() => {
+    getOneUser();
+  }, []);
 
   // Appel axios pour mettre à jour le freelancer avec ses informations et le user associé si profil complet
 
   const updateFreelancerCompletedProfile = (e) => {
     e.preventDefault();
-    backendAPI.get(`/api/freelancers/${freelancerId}/user`).then(() => {
-      // const userId = response.data.id;
-      backendAPI
-        .put(`/api/freelancers/${freelancerId}`, {
-          displayName,
-          activityDescription: activityPro,
-          zipCode: cityPro,
-          phone: phonePro,
-          experienceYear: experienceYearPro,
-          price: pricePro,
-          description: descriptionPro,
-          acceptEmails: acceptEmailPro,
-          siret: siretPro,
-          available: false,
-          picture: picturePro,
-        })
-        .then((response) => {
-          // if (result) {
-          //   backendAPI.put(`/api/users/${userId}`, {
-          //     profileIsComplete: true,
-          //   });
-          // }
-          if (response) {
-            toast({
-              title: "Vos données ont bien été enregistrées.",
-              description: "Bienvenue sur votre profil !",
-              status: "success",
-              duration: 7000,
-              position: "bottom-right",
-              isClosable: true,
-            });
-            navigate(`/profil/${freelancerId}`);
-          }
-        })
-        .catch((error) => {
-          if (error) {
-            toast({
-              title: "Veuillez renseigner tous les champs obligatoires",
-              status: "error",
-              duration: 7000,
-              position: "bottom-right",
-              isClosable: true,
-            });
-          }
-          console.warn(error);
+    const userId = user.id;
+    backendAPI
+      .put(`/api/freelancers/${freelancerId}`, {
+        displayName,
+        activityDescription: activityPro,
+        zipCode: cityPro,
+        phone: phonePro,
+        experienceYear: experienceYearPro,
+        price: pricePro,
+        description: descriptionPro,
+        acceptEmails: acceptEmailPro,
+        siret: siretPro,
+        available: false,
+        // picture: picturePro,
+      })
+      .then((response) => {
+        backendAPI.put(`/api/users/${userId}`, {
+          profileIsComplete: !user.profileIsComplete,
         });
-    });
+        if (response) {
+          toast({
+            title: "Vos données ont bien été enregistrées.",
+            description: "Bienvenue sur votre profil !",
+            status: "success",
+            duration: 7000,
+            position: "bottom-right",
+            isClosable: true,
+          });
+          navigate(`/profil/${freelancerId}`);
+        }
+      })
+      .catch((error) => {
+        if (error) {
+          toast({
+            title: "Veuillez renseigner tous les champs obligatoires",
+            status: "error",
+            duration: 7000,
+            position: "bottom-right",
+            isClosable: true,
+          });
+        }
+        console.warn(error);
+      });
   };
 
   // Appel axios pour mettre à jour le freelancer avec ses informations si profil incomplet
@@ -169,7 +155,7 @@ export default function ProAccountForm() {
         acceptEmails: acceptEmailPro,
         siret: siretPro,
         available: false,
-        picture: picturePro,
+        // picture: picturePro,
       })
       .then((response) => {
         if (response) {
@@ -307,104 +293,7 @@ export default function ProAccountForm() {
                 />
               </VStack>
             </FormControl>
-            <VStack align="center" alignSelf="center" mx="auto">
-              <Avatar src={picturePro} size="2xl" />
-              <Button
-                bg="none"
-                _hover={{ bg: "none" }}
-                color="pink.light"
-                fontWeight="800"
-                align="center"
-                fontSize={{ base: "md", md: "0.8rem" }}
-                onClick={onOpen}
-              >
-                {" "}
-                Changer votre photo
-              </Button>
-              <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent
-                  w="80%"
-                  maxW="800px"
-                  minH="500px"
-                  h="fix-content"
-                  borderRadius="25px"
-                >
-                  <ModalHeader
-                    paddingY="30px"
-                    color="purple.average"
-                    fontWeight="600"
-                    fontSize="lg"
-                    bgColor="#FAFAFA"
-                  >
-                    Votre plus beau profil
-                    <ModalCloseButton
-                      _hover={{ bgColor: "none" }}
-                      size="lg"
-                      iconColor="#415161"
-                      p="30px"
-                    />
-                  </ModalHeader>
-                  <ModalBody>
-                    <FormControl>
-                      <Box h="150px" alignSelf="left" my="4rem">
-                        <Box
-                          border="3px solid"
-                          borderColor="pink.light"
-                          position="absolute"
-                          h="150px"
-                          w="150px"
-                          mx="40%"
-                        />
-                        <Box h="150px" w="150px" mx="40%">
-                          <Image
-                            src="https://i.pravatar.cc/300"
-                            m="auto"
-                            h="150px"
-                          />
-                        </Box>
-                      </Box>
-                      <Flex
-                        direction="column"
-                        alignItems="center"
-                        mt="3rem"
-                        gap="5"
-                      >
-                        <Button
-                          type="file"
-                          bg="none"
-                          _hover={{ bg: "none", color: "pink.light" }}
-                          color="gray"
-                          fontWeight="600"
-                          align="center"
-                          fontSize={{ base: "md", md: "0.8rem" }}
-                          onClick={() =>
-                            document.getElementById("inputHandler").click()
-                          }
-                        >
-                          <Input
-                            type="file"
-                            id="inputHandler"
-                            name="name"
-                            display="none"
-                          />{" "}
-                          Télécharger votre nouvelle photo
-                        </Button>
-                        <Button
-                          w="120px"
-                          variant="solid_PrimaryColor"
-                          type="submit"
-                          fontSize="sm"
-                          onClick={handleRegisterPicture}
-                        >
-                          Enregistrer
-                        </Button>
-                      </Flex>
-                    </FormControl>
-                  </ModalBody>
-                </ModalContent>
-              </Modal>
-            </VStack>
+            <PictureProfilePro user={user} />
           </Flex>
           <FormControl>
             <Flex direction="column" rowGap="5" mt="1rem">
