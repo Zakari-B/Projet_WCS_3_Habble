87fc6b773ce7ef681e86d8418a978a4ca1f07bfb
Julie Combeau <combeau.julie@gmail.com>
2022-07-07 08:59:31 +0200

    add state and constante to get default value on checkbox

--- frontend/src/components/ProAccountForm/Expertises.jsx
+++ frontend/src/components/ProAccountForm/Expertises.jsx
@@ -16,6 +16,39 @@ export default function Expertises() {
   // useState pour chaque input //
   const [expertise, setExpertise] = useState([]);
   const [expertiseList, setExpertiseList] = useState([]);
+  const [expertiseListFiltered, setExpertiseListFiltered] = useState([]);
+
+  // Constante valeur par défaut //
+
+  const elderlyCare = expertiseListFiltered.filter((e) =>
+    ["1", "2"].includes(e)
+  );
+  const chronicDiseases = expertiseListFiltered.filter((e) =>
+    ["3", "4", "5", "6", "7"].includes(e)
+  );
+  const handicap = expertiseListFiltered.filter((e) =>
+    [
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+    ].includes(e)
+  );
+  const mentalHealth = expertiseListFiltered.filter((e) =>
+    ["24", "25", "26", "27", "28", "29", "30", "31", "32", "33"].includes(e)
+  );
 
   // fonction retrait et d'ajout d'une expertise //
   const updateExpertise = (e) => {
@@ -56,6 +89,9 @@ export default function Expertises() {
         setExpertiseList(
           response.data.map((e) => e.fk_expertise_id.id.toString())
         );
+        setExpertiseListFiltered(
+          response.data.map((e) => e.fk_expertise_id.id.toString())
+        );
       })
       .catch((error) => {
         console.warn(error);
@@ -85,19 +121,21 @@ export default function Expertises() {
       >
         Soins aux personnes agées
       </FormLabel>
-      <CheckboxGroup>
-        <Flex
-          justifyContent="left"
-          columnGap="3"
-          rowGap="2"
-          flexWrap="wrap"
-          h="fit-content"
-          w="fit-content%"
-        >
-          {expertise.map(
-            (element) =>
-              element.category === "Soins aux personnes agées" && (
+
+      <Flex
+        justifyContent="left"
+        columnGap="3"
+        rowGap="2"
+        flexWrap="wrap"
+        h="fit-content"
+        w="fit-content%"
+      >
+        {expertise.map(
+          (element) =>
+            element.category === "Soins aux personnes agées" && (
+              <CheckboxGroup defaultValue={elderlyCare}>
                 <Checkbox
+                  defaultChecked
                   iconColor="pink.light"
                   colorScheme="white"
                   borderColor="gray"
@@ -107,10 +145,11 @@ export default function Expertises() {
                 >
                   <Text fontSize="sm">{element.name}</Text>
                 </Checkbox>
-              )
-          )}
-        </Flex>
-      </CheckboxGroup>
+              </CheckboxGroup>
+            )
+        )}
+      </Flex>
+
       <FormLabel
         htmlFor="chronicDiseases"
         fontSize="sm"
@@ -119,18 +158,18 @@ export default function Expertises() {
       >
         Maladies chroniques
       </FormLabel>
-      <CheckboxGroup>
-        <Flex
-          justifyContent="left"
-          columnGap="3"
-          rowGap="2"
-          flexWrap="wrap"
-          h="fit-content"
-          w="fit-content%"
-        >
-          {expertise.map(
-            (element) =>
-              element.category === "Maladies chroniques" && (
+      <Flex
+        justifyContent="left"
+        columnGap="3"
+        rowGap="2"
+        flexWrap="wrap"
+        h="fit-content"
+        w="fit-content%"
+      >
+        {expertise.map(
+          (element) =>
+            element.category === "Maladies chroniques" && (
+              <CheckboxGroup defaultValue={chronicDiseases}>
                 <Checkbox
                   iconColor="pink.light"
                   colorScheme="white"
@@ -141,10 +180,10 @@ export default function Expertises() {
                 >
                   <Text fontSize="sm">{element.name}</Text>
                 </Checkbox>
-              )
-          )}
-        </Flex>
-      </CheckboxGroup>
+              </CheckboxGroup>
+            )
+        )}
+      </Flex>
       <FormLabel
         htmlFor="Disability"
         fontSize="sm"
@@ -153,18 +192,18 @@ export default function Expertises() {
       >
         Handicap
       </FormLabel>
-      <CheckboxGroup>
-        <Flex
-          justifyContent="left"
-          columnGap="3"
-          rowGap="2"
-          flexWrap="wrap"
-          h="fit-content"
-          w="fit-content%"
-        >
-          {expertise.map(
-            (element) =>
-              element.category === "Handicap" && (
+      <Flex
+        justifyContent="left"
+        columnGap="3"
+        rowGap="2"
+        flexWrap="wrap"
+        h="fit-content"
+        w="fit-content%"
+      >
+        {expertise.map(
+          (element) =>
+            element.category === "Handicap" && (
+              <CheckboxGroup defaultValue={handicap}>
                 <Checkbox
                   iconColor="pink.light"
                   colorScheme="white"
@@ -175,10 +214,10 @@ export default function Expertises() {
                 >
                   <Text fontSize="sm">{element.name}</Text>
                 </Checkbox>
-              )
-          )}
-        </Flex>
-      </CheckboxGroup>
+              </CheckboxGroup>
+            )
+        )}
+      </Flex>
       <FormLabel
         htmlFor="mentalHealth"
         fontSize="sm"
@@ -187,18 +226,18 @@ export default function Expertises() {
       >
         Santé mentale
       </FormLabel>
-      <CheckboxGroup>
-        <Flex
-          justifyContent="left"
-          columnGap="3"
-          rowGap="2"
-          flexWrap="wrap"
-          h="fit-content"
-          w="fit-content%"
-        >
-          {expertise.map(
-            (element) =>
-              element.category === "Santé mentale" && (
+      <Flex
+        justifyContent="left"
+        columnGap="3"
+        rowGap="2"
+        flexWrap="wrap"
+        h="fit-content"
+        w="fit-content%"
+      >
+        {expertise.map(
+          (element) =>
+            element.category === "Santé mentale" && (
+              <CheckboxGroup defaultValue={mentalHealth}>
                 <Checkbox
                   iconColor="pink.light"
                   colorScheme="white"
@@ -209,10 +248,10 @@ export default function Expertises() {
                 >
                   <Text fontSize="sm">{element.name}</Text>
                 </Checkbox>
-              )
-          )}
-        </Flex>
-      </CheckboxGroup>
+              </CheckboxGroup>
+            )
+        )}
+      </Flex>
     </Flex>
   );
 }
