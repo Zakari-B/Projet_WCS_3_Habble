678ad7ab49a43e070db6db4ea2084b0cf27ea09a
Loris <45939615+lchastanet@users.noreply.github.com>
2022-07-08 14:23:23 +0200

    add env vars

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const router = require("./router");
+require("dotenv").config();
 
 const app = express();
 
